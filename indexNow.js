2edbbec28a6a4d0a16b18b49aeb08281762bb503
이지영 <ljzero5@gmail.com>
2025-08-05 15:38:04 +0900

    Update

--- indexNow.js
+++ indexNow.js
@@ -7,6 +7,8 @@ const config = {
   keyLocation: "https://superleek.github.io/b69d2da5f922b5482dbdb63fd4c5b9aa.txt",
   urlList: [
     "https://superleek.github.io/",
+    "https://superleek.github.io/index.html",
+    "https://superleek.github.io/counselor-list.html",
     "https://superleek.github.io/detail-1.html",
     "https://superleek.github.io/detail-2.html",
     "https://superleek.github.io/detail-3.html"
